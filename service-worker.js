5c3484a2aef22a1a08137472046100685b606bf2
jozadad1 <jozadad1@yahoo.co.uk>
2020-07-05 11:34:52 +0100

    Fix start URL to manifest.json

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,32 @@
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js')
-    .then(registration => {
-      console.log('SW registered with scope:', registration.scope);
-    })
-    .catch(err => {
-      console.error('Registration failed:', err);
-    });
-  });
-}
+const cacheName = 'cache-v1';
+const precacheResources = [
+  '/',
+  'index.html',
+  'styles/main.css'
+];
 
 self.addEventListener('install', event => {
-  console.log('Service worker installing...');
-  // Add a call to skipWaiting here
+  console.log('Service worker install event!');
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(precacheResources);
+      })
+  );
 });
 
 self.addEventListener('activate', event => {
-  console.log('Service worker activating...');
+  console.log('Service worker activate event!');
 });
 
 self.addEventListener('fetch', event => {
-  console.log('Fetching:', event.request.url);
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+    );
 });
