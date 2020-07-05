be04cac28c8e77b7099081d3943a6750e58d99ca
jozadad1 <jozadad1@yahoo.co.uk>
2020-07-05 10:42:21 +0100

    Add metadata to index.html and improve service worker

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,25 @@
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('service-worker.js')
     .then(registration => {
-      console.log('Service Worker is registered', registration);
+      console.log('SW registered with scope:', registration.scope);
     })
     .catch(err => {
       console.error('Registration failed:', err);
     });
   });
 }
+
+self.addEventListener('install', event => {
+  console.log('Service worker installing...');
+  // Add a call to skipWaiting here
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activating...');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetching:', event.request.url);
+});
